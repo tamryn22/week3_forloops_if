36876f8e654eb6b624c1569731bfb59aa70f6ab2
tamryn22 <125568338+tamryn22@users.noreply.github.com>
2023-05-09 12:50:32 +0200

    loops and ifs
    adding for loops and ifs

---
+++ js/index.js
@@ -0,0 +1,93 @@
+//console.log ("working")
+//let valOne = -5;
+
+//if (valOne >= 0){
+    //console.log("The number is Positive")
+//} else {
+    //console.log("This number is Negitive")
+//}
+ //let value =6;
+ //if (value % 2 ===0){
+    // 6 /2 =3.0
+    //7/2 =3.5 --- 0.5
+   // console.log ("The number is even")
+ //} else{
+    //console.log ("The number is Odd")
+ //}
+
+
+
+ //let str= "Hello ";
+
+ //if (str.length > 10){
+    //console.log ("string count over 10 and is " + str.length);
+ //} else{
+    //console.log ("string count  is not over 10 and is" + str.length );
+ //}
+
+ //let value =10;
+ ////if (value >5 && value < 15){
+    //console.log ("the number is between 5 and 15")
+ //} else {
+    //console.log("tHE NUMBER IS NOT BETWEEN 5 AND 15");
+// }
+
+//let age =17;
+//let user = "tamryn";
+
+//if( age > 18 || user === "tamryn"){
+   // console.log ("i pull up")
+//} else{
+    //console.log(" no way china!")
+//}
+
+//let value =10;
+//for (let i =0; i <= value; i++ ){
+    //console.log (i)
+//}
+
+//for (let i = 10; i >= 0; i--){
+    //console.log (i)
+//}
+
+//let total = 0;
+//for ( let i =0; i <=10; i++){
+    //total= total+ i
+    //total += i;   //block scoped verriable 
+//} console.log (total) //outside the {} to get the last total
+
+// let nameArray =["Mike", "James", "Tim","Greg","Annie","Susan", "Jacob"];
+// let isSusan =false;
+// let isJames = false
+// let isDave =false
+
+// for (let i = 0; i < nameArray.length; i ++){
+//     //console.log (nameArray[i] === "Susan") {//[1]= mike [2]=james
+//         if (nameArray [i] === "Susan"){
+//             isSusan = true;
+//         }
+//         if (nameArray [i] === "James"){
+//             isJames = true;
+//         }
+//         if (nameArray [i] === "Dave"){
+//             isDave = true;
+//         }
+// }
+// console.log ("Is Susan on the list? Answer :" + isSusan)
+// console.log ("Is Susan on the list? Answer :" + isJames)
+// console.log ("Is Susan on the list? Answer :" + isDave)
+//}
+
+// if (!isSusan){
+//     console.log ("Susan is not on the list")
+// } else {
+//     console.log("yes she is on the list")
+// }
+
+// let numberArray = [10, 50 , 83, 54, 32, 78, 65, 100];
+// let total=0;
+// for (let i =0; i < numberArray.length; i ++){
+//    // total+= numberArray [i];
+//     //console.log (total)
+//}
+//console.log (total)
